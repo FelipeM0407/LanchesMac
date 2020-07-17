7f7b0e3d7d83dc4971fc9aa8f02595f7aa836df4
Felipe Moreira <felipemoreirasilva97@gmail.com>
2020-07-17 04:23:43 -0300

    Upload dos demais arquivos

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,33 @@
+﻿using LanchesMac.Repositories;
+using LanchesMac.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+
+            //var lanches = _lancheRepository.Lanches;
+            //return View(lanches);
+
+            var lancheslistViewModel = new LancheListViewModel();
+            lancheslistViewModel.Lanches = _lancheRepository.Lanches;
+            lancheslistViewModel.CategoriaAtual = "Categoria Atual";
+
+            return View(lancheslistViewModel);
+        }
+    }
+}
